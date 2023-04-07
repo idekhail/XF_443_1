bc3cd614552e0ff4548467ddc045289e6fbc5f9f
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-04-08 02:04:43 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+namespace XF_443_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Ok.Clicked += (s, e) => Show.Text = Name.Text;
+            Clear.Clicked += (s, e) =>
+            {
+                Name.Text = string.Empty;
+                Show.Text = "Show";
+            };
+        }
+    }
+}
